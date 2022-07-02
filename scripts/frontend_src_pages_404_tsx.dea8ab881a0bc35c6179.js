211976302566cf60a08e407b3d7c297e14bcd11e
aihara-sama <aihara-sama@mail.ru>
2022-07-02 19:20:08 +0300

    init commit

---
+++ scripts/frontend_src_pages_404_tsx.dea8ab881a0bc35c6179.js
@@ -0,0 +1,34 @@
+"use strict";
+(self["webpackChunknetwork"] = self["webpackChunknetwork"] || []).push([["frontend_src_pages_404_tsx"],{
+
+/***/ "./frontend/src/pages/404.tsx":
+/*!************************************!*\
+  !*** ./frontend/src/pages/404.tsx ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+
+
+var NotFound = function NotFound() {
+  react__WEBPACK_IMPORTED_MODULE_0___default().useEffect(function () {
+    document.title = '404';
+  }, []);
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("h1", {
+    children: "404"
+  });
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (NotFound);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=frontend_src_pages_404_tsx.dea8ab881a0bc35c6179.js.map
